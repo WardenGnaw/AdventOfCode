46a4598f8c6ee8d97109a8135c1928ac17174b83
Andrew Wang <waan@microsoft.com>
2021-12-16 21:00:20 -0800

    Advent of Code 2021 - Day 14/15

---
+++ 2021/Problems/Day15.cs
@@ -0,0 +1,222 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+
+namespace AdventOfCode2021.Day15
+{
+    public class Main
+    {
+        public class Chiton
+        {
+            public int RiskLevel;
+
+            public Chiton(int riskLevel)
+            {
+                RiskLevel = riskLevel;
+            }
+        }
+
+        private Dictionary<Tuple<int, int>, int> indexToRisk = new();
+
+        public int MinRisk(Chiton[,] chitons, int x, int y, int size)
+        {
+            if (indexToRisk.ContainsKey(Tuple.Create(x, y)))
+            {
+                return indexToRisk[Tuple.Create(x, y)];
+            }
+
+            int risk = chitons[x, y].RiskLevel;
+
+            if (x + 1 < size && y + 1 < size)
+            {
+                int down = MinRisk(chitons, x + 1, y, size);
+                int right = MinRisk(chitons, x, y + 1, size);
+
+                risk += Math.Min(down, right);
+            }
+            else if (x + 1 < size)
+            {
+                risk += MinRisk(chitons, x + 1, y, size);
+            }
+            else if (y + 1 < size)
+            {
+                risk += MinRisk(chitons, x, y + 1, size);
+            }
+
+
+            if (!indexToRisk.ContainsKey(Tuple.Create(x, y)))
+            {
+                indexToRisk[Tuple.Create(x, y)] = risk;
+            }
+
+            return risk;
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            string[] data = IO.ReadFile(Path.Combine(Directory.GetCurrentDirectory(), "input1.txt")).Split("\r\n");
+            Chiton[,] chitons = new Chiton[data.Length, data.Length];
+
+            int i = 0;
+            foreach (string d in data)
+            {
+                int j = 0;
+                int[] values = d.ToCharArray().Select(x => int.Parse(x.ToString())).ToArray();
+                foreach (int v in values)
+                {
+                    chitons.SetValue(new Chiton(v), i, j);
+                    j++;
+                }
+                i++;
+            }
+
+            int down = MinRisk(chitons, 1, 0, data.Length);
+            int right = MinRisk(chitons, 0, 1, data.Length);
+
+            Assert.Equal(0, Math.Min(down, right));
+        }
+
+        private Dictionary<Tuple<int, int>, int> grid = new();
+
+        public int MinRisk2(Chiton[,] chitons, int x, int y, int size, int expansion)
+        {
+            int actualX = x % size;
+            int actualY = y % size;
+
+            if (indexToRisk.ContainsKey(Tuple.Create(x, y)))
+            {
+                return indexToRisk[Tuple.Create(x, y)];
+            }
+
+            int riskValue = chitons[actualX, actualY].RiskLevel + x / size + y / size;
+
+            int sumRisk;
+            switch (riskValue)
+            {
+                case 1:
+                    sumRisk = 1;
+                    break;
+                case 2:
+                    sumRisk = 2;
+                    break;
+                case 3:
+                    sumRisk = 3;
+                    break;
+                case 4:
+                    sumRisk = 4;
+                    break;
+                case 5:
+                    sumRisk = 5;
+                    break;
+                case 6:
+                    sumRisk = 6;
+                    break;
+                case 7:
+                    sumRisk = 7;
+                    break;
+                case 8:
+                    sumRisk = 8;
+                    break;
+                case 9:
+                    sumRisk = 9;
+                    break;
+                case 10:
+                    sumRisk = 1;
+                    break;
+                case 11:
+                    sumRisk = 2;
+                    break;
+                case 12:
+                    sumRisk = 3;
+                    break;
+                case 13:
+                    sumRisk = 4;
+                    break;
+                case 14:
+                    sumRisk = 5;
+                    break;
+                case 15:
+                    sumRisk = 6;
+                    break;
+                case 16:
+                    sumRisk = 7;
+                    break;
+                case 17:
+                    sumRisk = 8;
+                    break;
+                default:
+                    throw new Exception("Past Max Value");
+            };
+            grid[Tuple.Create(x, y)] = sumRisk;
+
+            Debug.Assert(x < 500 && y < 500);
+
+            if (x + 1 < (size * expansion) && y + 1 < (size * expansion))
+            {
+                int down = MinRisk2(chitons, x + 1, y, size, expansion);
+                int right = MinRisk2(chitons, x, y + 1, size, expansion);
+
+                sumRisk += Math.Min(down, right);
+            }
+            else if (x + 1 < (size * expansion))
+            {
+                sumRisk += MinRisk2(chitons, x + 1, y, size, expansion);
+            }
+            else if (y + 1 < (size * expansion))
+            {
+                sumRisk += MinRisk2(chitons, x, y + 1, size, expansion);
+            }
+
+
+            if (!indexToRisk.ContainsKey(Tuple.Create(x, y)))
+            {
+                indexToRisk[Tuple.Create(x, y)] = sumRisk;
+            }
+
+            return sumRisk;
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            string[] data = IO.ReadFile(Path.Combine(Directory.GetCurrentDirectory(), "input1.txt")).Split("\r\n");
+            Chiton[,] chitons = new Chiton[data.Length, data.Length];
+
+            int i = 0;
+            foreach (string d in data)
+            {
+                int j = 0;
+                int[] values = d.ToCharArray().Select(x => int.Parse(x.ToString())).ToArray();
+                foreach (int v in values)
+                {
+                    chitons.SetValue(new Chiton(v), i, j);
+                    j++;
+                }
+                i++;
+            }
+
+            int down = MinRisk2(chitons, 1, 0, data.Length, 5);
+            int right = MinRisk2(chitons, 0, 1, data.Length, 5);
+
+            //for (int j = 0; j < data.Length * 5; j++)
+            //{
+            //    for (int k = 0; k < data.Length * 5; k++)
+            //    {
+            //        if (grid.ContainsKey(Tuple.Create(j, k)))
+            //        {
+            //            Debug.Write(grid[Tuple.Create(j, k)]);
+            //        }
+            //        else
+            //        {
+            //            Debug.Write(chitons[j, k].RiskLevel);
+            //        }
+            //    }
+            //    Debug.WriteLine("");
+            //}
+
+            Assert.Equal(0, Math.Min(down, right));
+        }
+    }
+}
---
+++ 2021/Problems/Day16.cs
@@ -0,0 +1,295 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using Utilities;
+
+namespace AdventOfCode2021.Day16
+{
+    public abstract class Packet
+    {
+        public byte Version;
+
+        public byte TypeId { get; }
+
+        public Packet(byte version, byte typeId)
+        {
+            Version = version;
+            TypeId = typeId;
+        }
+
+        public static Packet CreatePacket(BitStreamReader reader)
+        {
+            Packet packet = null;
+            byte version = reader.ReadByte(3);
+            byte packetId = reader.ReadByte(3);
+            switch (packetId)
+            {
+                case 0:
+                    packet = new SumPacket(version, packetId, reader);
+                    break;
+                case 1:
+                    packet = new ProductPacket(version, packetId, reader);
+                    break;
+                case 2:
+                    packet = new MinimumPacket(version, packetId, reader);
+                    break;
+                case 3:
+                    packet = new MaximumPacket(version, packetId, reader);
+                    break;
+                case 4:
+                    packet = new LiteralValuePacket(version, packetId, reader);
+                    break;
+                case 5:
+                    packet = new GreaterThanPacket(version, packetId, reader);
+                    break;
+                case 6:
+                    packet = new LessThanPacket(version, packetId, reader);
+                    break;
+                case 7:
+                    packet = new EqualToPacket(version, packetId, reader);
+                    break;
+                default:
+                    throw new InvalidOperationException("Not a valid packet type");
+            }
+
+            return packet;
+        }
+
+        public abstract ulong Value { get; }
+    }
+
+    public class LiteralValuePacket : Packet
+    {
+        public override ulong Value { get; }
+
+        public LiteralValuePacket(byte version, byte typeId, BitStreamReader reader) : base(version, typeId)
+        {
+            Value = 0;
+
+            while (reader.ReadBit())
+            {
+                Value = Value << 4;
+                Value |= reader.ReadByte(4);
+            }
+            Value = Value << 4;
+            Value |= reader.ReadByte(4);
+        }
+    }
+
+    public class OperatorPacket : Packet
+    {
+        public override ulong Value { get; }
+
+        public List<Packet> Packets;
+
+        public OperatorPacket(byte version, byte typeId, BitStreamReader reader) : base(version, typeId)
+        {
+            Value = 0;
+
+            Packets = new List<Packet>();
+
+            bool lengthTypeId = reader.ReadBit();
+            if (lengthTypeId)
+            {
+                int numPackets = reader.ReadByte(8) << 8 | reader.ReadByte(3);
+
+                for (int i = 0; i < numPackets; i++)
+                {
+                    Packets.Add(CreatePacket(reader));
+                }
+            }
+            else
+            {
+                int numBits = reader.ReadByte(8) << 7;
+                    numBits |= reader.ReadByte(7);
+                List<byte> bytes = new List<byte>();
+                int i;
+                for (i = numBits; i >= 8; i -= 8)
+                {
+                    bytes.Add(reader.ReadByte(8));
+                }
+                if (i > 0)
+                {
+                    bytes.Add((byte)((byte)reader.ReadByte(i) << (8 - i)));
+                }
+                BitStreamReader innerReader = new BitStreamReader(bytes.ToArray(), (uint)numBits);
+                while (!innerReader.EndOfStream)
+                {
+                    Packets.Add(Packet.CreatePacket(innerReader));
+                }
+            }
+        }
+    }
+
+    public class SumPacket : OperatorPacket
+    {
+        public override ulong Value { 
+            get
+            {
+                ulong sum = 0;
+                foreach (Packet p in Packets)
+                {
+                    sum += p.Value;
+                }
+                return sum;
+            }
+        }
+
+        public SumPacket(byte version, byte typeId, BitStreamReader reader) : base(version, typeId, reader)
+        {
+        }
+    }
+
+    public class ProductPacket : OperatorPacket
+    {
+        public override ulong Value
+        {
+            get
+            {
+                return Packets.Aggregate((ulong)1, (x, y) => x * y.Value);
+            }
+        }
+
+        public ProductPacket(byte version, byte typeId, BitStreamReader reader) : base(version, typeId, reader)
+        {
+        }
+    }
+
+    public class MinimumPacket : OperatorPacket
+    {
+        public override ulong Value
+        {
+            get
+            {
+                return Packets.Min(x => x.Value);
+            }
+        }
+
+        public MinimumPacket(byte version, byte typeId, BitStreamReader reader) : base(version, typeId, reader)
+        {
+        }
+    }
+
+    public class MaximumPacket : OperatorPacket
+    {
+        public override ulong Value
+        {
+            get
+            {
+                return Packets.Max(x => x.Value);
+            }
+        }
+
+        public MaximumPacket(byte version, byte typeId, BitStreamReader reader) : base(version, typeId, reader)
+        {
+        }
+    }
+
+    public class GreaterThanPacket : OperatorPacket
+    {
+        public override ulong Value
+        {
+            get
+            {
+                return Packets[0].Value > Packets[1].Value ? (ulong)1 : (ulong)0;
+            }
+        }
+
+        public GreaterThanPacket(byte version, byte typeId, BitStreamReader reader) : base(version, typeId, reader)
+        {
+        }
+    }
+
+    public class LessThanPacket : OperatorPacket
+    {
+        public override ulong Value
+        {
+            get
+            {
+                return Packets[0].Value < Packets[1].Value ? (ulong)1 : (ulong)0;
+            }
+        }
+
+        public LessThanPacket(byte version, byte typeId, BitStreamReader reader) : base(version, typeId, reader)
+        {
+        }
+    }
+
+    public class EqualToPacket : OperatorPacket
+    {
+        public override ulong Value
+        {
+            get
+            {
+                return Packets[0].Value == Packets[1].Value ? (ulong)1 : (ulong)0;
+            }
+        }
+
+        public EqualToPacket(byte version, byte typeId, BitStreamReader reader) : base(version, typeId, reader)
+        {
+        }
+    }
+
+    public class Main
+    {
+        [Fact]
+        public void Part1()
+        {
+            string data = IO.ReadFile(Path.Combine(Directory.GetCurrentDirectory(), "input1.txt"));
+            List<byte> packet = new List<byte>();
+
+            for (int i = 1; i < data.Length; i += 2)
+            {
+                string byteStr = string.Concat(data[i - 1], data[i]);
+                packet.Add(byte.Parse(byteStr, NumberStyles.HexNumber));
+            }
+
+            List<Packet> packets = new List<Packet>();
+
+            BitStreamReader reader = new BitStreamReader(packet.ToArray());
+
+            ulong sumVersion = 0;
+            Packet top = Packet.CreatePacket(reader);
+            Queue<Packet> queue = new Queue<Packet>();
+            queue.Enqueue(top);
+            while (queue.Count > 0)
+            {
+                Packet p = queue.Dequeue();
+                sumVersion += p.Version;
+                if (p is OperatorPacket op)
+                {
+                    foreach (Packet inner in op.Packets)
+                    {
+                        queue.Enqueue(inner);
+                    }
+                }
+
+            }
+
+            Assert.Equal((ulong)0, sumVersion);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            string data = IO.ReadFile(Path.Combine(Directory.GetCurrentDirectory(), "input1.txt"));
+            List<byte> packet = new List<byte>();
+
+            for (int i = 1; i < data.Length; i += 2)
+            {
+                string byteStr = string.Concat(data[i - 1], data[i]);
+                packet.Add(byte.Parse(byteStr, NumberStyles.HexNumber));
+            }
+
+            List<Packet> packets = new List<Packet>();
+
+            BitStreamReader reader = new BitStreamReader(packet.ToArray());
+
+            ulong sumVersion = 0;
+            Packet top = Packet.CreatePacket(reader);
+
+
+            Assert.Equal((ulong)0, top.Value);
+        }
+    }
+}
